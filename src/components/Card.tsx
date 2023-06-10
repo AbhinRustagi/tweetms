bbe021e14740fa1f0889ff7dc6b7b467ec3e648e
AbhinRustagi <rustagi.abhin@gmail.com>
2023-06-10 11:20:04 +0530

    feat: add card

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+export const Card = () => {
+  return (
+    <div className="rounded-lg border-stone-200 border border-solid p-8 bg-white w-80 hover:bg-sky-500 hover:text-white cursor-pointer">
+      <p className="font-bold">Abhin Rustagi</p>
+      <p className="font-light text-gray-500">@abhinrustagi</p>
+      <p className="leading-relaxed text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, sint
+        minima. Reprehenderit ea atque aut, maxime voluptatum voluptates,
+        recusandae et, doloribus necessitatibus quaerat aliquam fugiat a sunt
+        odio quas. Soluta!
+      </p>
+      <p>23:01 PM</p>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Card } from "@/components/Card";
+import React from "react";
+
 export default function Home() {
   return (
-    <>
+    <main>
       <p className="text-red-500">hello</p>
-    </>
+      <Card />
+    </main>
   );
 }
+
+// export async function getServerSideProps() {
+//   const response = await fetch("/api/getPostedTweets");
+//   const data = response.json();
+
+//   return {
+//     props: {
+//       tweets: [],
+//     },
+//   };
+// }
