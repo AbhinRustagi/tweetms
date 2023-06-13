5edcb47c5c668097045c96c3026b3ba20efa65e7
AbhinRustagi <rustagi.abhin@gmail.com>
2023-06-13 21:58:39 +0530

    feat: add docs page

---
+++ src/pages/docs.tsx
@@ -0,0 +1,5 @@
+function Docs() {
+  return <main></main>;
+}
+
+export default Docs;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { Card, Hero, Meta } from "@/components";
 import { cardDefaultProps } from "@/data";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Meta />
+      <header className="my-4 flex justify-end max-w-4xl lg:px-0 px-6 mx-auto">
+        <Link
+          href="/docs"
+          className="rounded-lg px-4 py-2 text-sm bg-sky-200/75 text-sky-700 hover:bg-sky-300/75 font-semibold"
+        >
+          Docs
+        </Link>
+      </header>
       <main className="max-w-4xl lg:px-0 px-6 mx-auto py-6">
         <Hero />
         <Card {...cardDefaultProps} />
